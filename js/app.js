524bfc13d1da63c03757d04d51e2c26f781e9132
IAS <is@daypaio.com>
2020-03-13 15:34:55 +0100

    js

--- js/app.js
+++ js/app.js
@@ -5,12 +5,15 @@ const phrase = document.getElementById('phrase');
 let missed = 0;
 const startButton = document.getElementsByClassName('btn__reset');
 
+
+// listens to the start button being clicked
 startButton.addEventListener('click', () => {
     alert('stuff');
     let overlay = document.getElementById('overlay');
     overlay.style.display = 'none';
 });
 
+// set of complete phrases
 const phrases = [
     'Change the world by being yourself',
     'Aspire to inspire before we expire',
@@ -19,8 +22,11 @@ const phrases = [
     'Daypaio experienced an unexpected downtime'
 ]
 
-function getRandomPhraseAsArray(arr){
-    //do stuff to any arr that is passed in 
+
+
+function getRandomPhraseAsArray(arr) {
+    //do stuff to any arr that is passed in
+    let randomNumber = Math.random
 } 
 
 //getRandomPhraseAsArray(phrases);
