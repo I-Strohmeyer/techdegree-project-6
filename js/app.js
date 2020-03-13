4bfc49c0118ce90c6d66039703f5de3293bfd0f6
IAS <is@daypaio.com>
2020-03-13 16:24:00 +0100

    hide screen when start click

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
-// alert('Hello world');
-
+// Variables
 const qwerty = document.getElementById('querty');
 const phrase = document.getElementById('phrase');
 let missed = 0;
-const startButton = document.getElementsByClassName('btn__reset');
+const startButton = document.getElementsByClassName('btn__reset')[0];
 
 
 // listens to the start button being clicked
 startButton.addEventListener('click', () => {
-    alert('stuff');
     let overlay = document.getElementById('overlay');
     overlay.style.display = 'none';
 });
@@ -20,14 +18,29 @@ const phrases = [
     'Strive for greatness',
     'To live will be an awfully big adventure',
     'Daypaio experienced an unexpected downtime'
-]
+];
 
-
+// get random number between a min and max number 
+function getRandomInt(min, max) {
+    min = Math.floor(min);
+    max = Math.ceil(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function getRandomPhraseAsArray(arr) {
-    //do stuff to any arr that is passed in
-    let randomNumber = Math.random
+    //get a random number between 0 and the length of the array
+    let randomNumber = getRandomInt(0, phrases.length-1);
+
+    // get the string based on that random number
+    let randomPhrase = phrases[randomNumber];
+
+    // get an arrary of characters from that string
+    return randomPhrase.split('');
 } 
+
+const characterArray = getRandomPhraseAsArray(phrases);
+console.log(characterArray);
+
 
 //getRandomPhraseAsArray(phrases);
 
